c0089cf86abe295bd9c6e94408409e9d0be1a63e
guy-jerome <aaronjamesroberts@gmail.com>
2025-06-29 15:35:01 -0400

    Vite config changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/many-maps/', // Set base for GitHub Pages
+  base: "/many-maps/", // Set base for GitHub Pages
   plugins: [react()],
-})
+});
