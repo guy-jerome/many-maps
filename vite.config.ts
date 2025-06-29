bd7705dd2a6e0b39d20b36d8ba002d3a459ecc7b
guy-jerome <aaronjamesroberts@gmail.com>
2025-06-29 15:18:45 -0400

    pages added

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/many-maps/', // Set base for GitHub Pages
   plugins: [react()],
 })
